cec8d13c7c1b22196f73c304485ab7a781136614
Assaf Berman <Assaf.Berman@s.afeka.ac.il>
2023-01-20 15:45:54 +0200

    hyper parameter tuning

--- main.py
+++ main.py
@@ -1,61 +1,121 @@
-import tensorflow as tf
+import datetime
+
 import keras
+import tensorflow as tf
+from keras.layers import Conv2D, Add, BatchNormalization, Input, Dropout, MaxPooling2D, Flatten, Dense
 from keras.optimizers import Adam
-from keras.losses import CategoricalCrossentropy, KLDivergence
+from keras.losses import CategoricalCrossentropy
 from keras.metrics import CategoricalAccuracy
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import confusion_matrix
+from keras.preprocessing.image import ImageDataGenerator
+import csv
+from tensorboard.plugins.hparams import api as hp
 
+print(tf.__version__)
+print(tf.config.list_physical_devices())
 (cifar100_image_train, cifar100_label_train), (
     cifar100_image_test, cifar100_label_test) = tf.keras.datasets.cifar100.load_data()
 encoder = OneHotEncoder(sparse_output=False)
 cifar100_label_train_encoded = encoder.fit_transform(cifar100_label_train)
 cifar100_label_test_encoded = encoder.transform(cifar100_label_test)
+cifar100_image_train = cifar100_image_train / 255.0
+cifar100_image_test = cifar100_image_test / 255.0
+datagen = ImageDataGenerator(rotation_range=15, width_shift_range=0.2, height_shift_range=0.2,
+                             horizontal_flip=True, vertical_flip=False, validation_split=0.2)
+datagen.fit(cifar100_image_train)
 
-print(cifar100_image_train.shape)
-print(cifar100_label_train.shape)
-print(cifar100_label_train_encoded.shape)
-print(type(cifar100_image_train))
-print(type(cifar100_label_train.shape))
-print(type(cifar100_label_train_encoded.shape))
+HP_NUM_RCLS = hp.HParam('num_of_RCLs', hp.Discrete(range(4, 8)))
+HP_NUM_CONVS = hp.HParam('num_of_convs', hp.Discrete(range(2, 6)))
+METRIC_ACCURACY = hp.Metric('categorical_accuracy', display_name='categorical_accuracy')
+METRIC_CROSSENTROPY = hp.Metric('categorical_crossentropy', display_name='categorical_crossentropy')
 
-NUM_OF_RCLS = 4
-NUM_OF_CONVS_IN_RCL = 3
+with tf.summary.create_file_writer('logs/hparam_tuning').as_default():
+    hp.hparams_config(hparams=[HP_NUM_RCLS, HP_NUM_CONVS], metrics=[METRIC_ACCURACY, METRIC_CROSSENTROPY])
 
-input_layer = tf.keras.layers.Input(shape=(32, 32, 3))
-conv1 = tf.keras.layers.Conv2D(filters=192, kernel_size=(5, 5))(input_layer)
-output = tf.keras.layers.BatchNormalization()(conv1)
-for i in range(NUM_OF_RCLS):
-    conv2 = tf.keras.layers.Conv2D(filters=192, kernel_size=(3, 3))(output)
-    bn = tf.keras.layers.BatchNormalization()(conv2)
-    x = bn
-    for j in range(NUM_OF_CONVS_IN_RCL):
-        conv2 = tf.keras.layers.Conv2D(filters=192, kernel_size=(3, 3), padding='same')(bn)
-        rcl2 = tf.keras.layers.Add()([x, conv2])
-        bn = tf.keras.layers.BatchNormalization()(rcl2)
-    if i == 1:
-        output = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(bn)
-    if i != NUM_OF_CONVS_IN_RCL - 1:
-        output = tf.keras.layers.Dropout(rate=0.8)(bn)
-global_maxpool = tf.keras.layers.GlobalMaxPooling2D()(bn)
-output_layer = tf.keras.layers.Dense(units=100)(global_maxpool)
 
-model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
-model.compile(optimizer=Adam(learning_rate=0.01), loss=KLDivergence(),
-              metrics=CategoricalAccuracy())
-model.summary()
-# tf.keras.utils.plot_model(model, to_file='./model_graph.png', show_shapes=True, show_layer_activations=True)
+def RCL_block(block_input, hparams):
+    conv1 = Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(block_input)
+    stack2 = BatchNormalization()(conv1)
 
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=1)
-early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True,
-                                                           min_delta=0.001, verbose=1)
-reduce_lr_callback = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_delta=0.001,
-                                                          verbose=1)
-callbacks = [tensorboard_callback, early_stopping_callback, reduce_lr_callback]
-model_fit = model.fit(cifar100_image_train, cifar100_label_train_encoded, epochs=50, batch_size=128,
-                      validation_split=0.2, callbacks=callbacks)
+    RCL = Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')
+    conv2 = RCL(stack2)
+    stack3 = Add()([conv1, conv2])
+    stack4 = BatchNormalization()(stack3)
 
-model.save('./pre_trained/')
-y_pred = model.predict(cifar100_image_test)
-m = CategoricalAccuracy()
-m.update_state((cifar100_label_test_encoded, y_pred))
-print('Accuracy:', m.result()().numpy())
+    for num_of_convs in range(0, hparams[HP_NUM_CONVS] - 1):
+        conv2 = Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu',
+                       weights=RCL.get_weights())(stack4)
+        stack3 = Add()([conv1, conv2])
+        stack4 = BatchNormalization()(stack3)
+
+    return stack4
+
+
+def train_test_model(hparams):
+    input_img = Input(shape=(32, 32, 3))
+    rconv1 = Conv2D(filters=64, kernel_size=[5, 5], strides=(1, 1), padding='same', activation='relu')(input_img)
+
+    for num_of_rcls in range(hparams[HP_NUM_RCLS]):
+        rconv1 = RCL_block(block_input=rconv1, hparams=hparams)
+        if num_of_rcls != hparams[HP_NUM_RCLS] - 1:
+            rconv1 = Dropout(rate=0.2)(rconv1)
+        if num_of_rcls == 1:
+            rconv1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(rconv1)
+
+    out = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(rconv1)
+    flatten = Flatten()(out)
+    prediction = Dense(units=100, activation='softmax')(flatten)
+
+    model = tf.keras.Model(inputs=input_img, outputs=prediction)
+    model.compile(optimizer=Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08),
+                  loss=CategoricalCrossentropy(),
+                  metrics=CategoricalAccuracy())
+    early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5,
+                                                               min_delta=0.001, verbose=1)
+    reduce_lr_callback = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3,
+                                                              min_delta=0.001,
+                                                              verbose=1)
+    callbacks = [early_stopping_callback, reduce_lr_callback]
+    model.fit(
+        datagen.flow(cifar100_image_train, cifar100_label_train_encoded, batch_size=32, subset='training'),
+        validation_data=datagen.flow(cifar100_image_train, cifar100_label_train_encoded, batch_size=8,
+                                     subset='validation'), epochs=50, batch_size=32, callbacks=callbacks)
+    model.save(f'./pre_trained/RCL_{hparams[HP_NUM_RCLS]}_{hparams[HP_NUM_CONVS]}/')
+    eval_loss, eval_accuracy = model.evaluate(cifar100_image_test, cifar100_label_test_encoded)
+    return eval_loss, eval_accuracy
+
+
+def run(run_dir, hparams):
+    with tf.summary.create_file_writer(run_dir).as_default():
+        hp.hparams(hparams)
+        loss, accuracy = train_test_model(hparams)
+        tf.summary.scalar('categorical_crossentropy', loss, step=1)
+        tf.summary.scalar('categorical_accuracy', accuracy, step=1)
+
+
+session_num = 8
+
+for num_rcls in HP_NUM_RCLS.domain.values:
+    for num_convs in HP_NUM_CONVS.domain.values:
+        hparams = {
+            HP_NUM_RCLS: num_rcls,
+            HP_NUM_CONVS: num_convs
+        }
+        run_name = f'run-{session_num}'
+        print(f'--- Starting trial: {run_name}')
+        print({h.name: hparams[h] for h in hparams})
+        run(f'logs/hparam_tuning/{run_name}', hparams)
+        session_num += 1
+
+# model = keras.models.load_model('pre_trained/RCL_4_5')
+# y_pred = model.predict(cifar100_image_test)
+# m = CategoricalAccuracy()
+# m.update_state(cifar100_label_test_encoded, y_pred)
+# print(f'Accuracy for {num_rcls} RCLs: {m.result().numpy()}')
+# print(f'Accuracy: {m.result().numpy()}')
+# print('Confusion matrix:')
+# conf_matrix = confusion_matrix(cifar100_label_test_encoded.argmax(axis=1), y_pred.argmax(axis=1))
+# with open('conf.csv','w+') as my_conf:
+#    csvWriter = csv.writer(my_conf, delimiter=',')
+#    csvWriter.writerows(conf_matrix)
